ac63fa79f865c2919ee096f0e75a247a55d6b1d3
PRED13 <jm0824621@gmail.com>
2026-05-07 08:51:28 -0600

    ss

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1234567890'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
